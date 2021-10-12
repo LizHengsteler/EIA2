2212d3655239801d06702601bba36542131003ee
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-10-12 18:10:13 +0200

    Übungen zu L01

---
+++ test.js
@@ -0,0 +1,24 @@
+/*let v: number = 1;
+v = v + "1";
+console.log(v);*/
+/*let a: [number] = [7, true, "Hallo"];
+a[4] = [101, 102];
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);*/
+var s = { "zahl": 7, "wahr": true, text: "Hallo" };
+console.log(s);
+console.log(s["key"]);
+console.log(s.key);
+function isDivisible(dividend, divisor) { return (dividend % divisor == 0); }
+;
+isDivisible(4, 3);
+isDivisible(4, 2);
+function test(_x) {
+    console.log(_x);
+}
+test(172);
+//# sourceMappingURL=test.js.map
