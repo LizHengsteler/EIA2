705335a1f5e50ad261b21f640f26cc165cc59338
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-10-13 15:59:22 +0200

    For-Schleife eingefügt und Variablen deklariert

--- Aufgaben/L01/RandomPoem.js
+++ Aufgaben/L01/RandomPoem.js
@@ -1,2 +1,13 @@
-console.log("Expecto Patronum");
+var L01;
+(function (L01) {
+    var subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    var verb = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    var object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    /*console.log(subject);
+    console.log(verb);
+    console.log(object);*/
+    for (var index = 6; index > 0; index--) {
+        console.log(index);
+    }
+})(L01 || (L01 = {}));
 //# sourceMappingURL=RandomPoem.js.map
--- Aufgaben/L01/RandomPoem.ts
+++ Aufgaben/L01/RandomPoem.ts
@@ -1,1 +1,18 @@
-console.log("Expecto Patronum");
+namespace L01 {
+
+
+    let subject: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verb: string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let object: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+    /*console.log(subject);
+    console.log(verb);
+    console.log(object);*/
+
+    for (let index: number = 6; index > 0; index--) {
+        console.log(index);
+
+    }
+
+
+}
