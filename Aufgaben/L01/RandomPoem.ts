9bcfe65e2f94fe57950b84a5520a7c036d92aff6
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-10-14 12:01:28 +0200

    Leerzeichen entfernt

--- Aufgaben/L01/RandomPoem.ts
+++ Aufgaben/L01/RandomPoem.ts
@@ -1,55 +1,27 @@
 namespace L01 {
-
     //bearbeitet mit Lena, Kriss und Amélie
-
 
     let subject: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verb: string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let object: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
-    /*console.log(subject);
-    console.log(verb);
-    console.log(object);*/
-
+    //console.log(subject, verb, object);
+    
     for (let index: number = subject.length; index > 0; index--) {
 
         getVerse(subject, verb, object);
         //console.log(index);
-
     }
 
-    /*for (let index: number = verb.length; index > 0; index--) {
-        //console.log(index);
-
-    }
-
-    for (let index: number = object.length; index > 0; index--) {
-        //console.log(index);
-
-    }*/
-
-    function getVerse (_subject: string[], _verb: string[], _object: string[] ): void {
+    function getVerse (_subject: string[], _verb: string[], _object: string[] ): string {
         let vers: string = "";
         let randomNumberSubject: number = Math.floor(Math.random() * _subject.length);
         let randomNumberVerb: number = Math.floor(Math.random() * _subject.length);
         let randomNumberObject: number = Math.floor(Math.random() * _subject.length);
         vers = vers + _subject.splice(randomNumberSubject, 1)[0] + " " +  _verb.splice(randomNumberVerb, 1) + " " + _object.splice(randomNumberObject, 1);
         console.log(vers);
-        
-
         //console.log(_subject);
-
         //console.log(randomNumberSubject);
-
-
-        //return("Alohomora");
-        
-    }
-
-    //console.log(getVerse);
-
-
-    
-
-
+        return("Alohomora");   
+    } //console.log(getVerse);
 }
