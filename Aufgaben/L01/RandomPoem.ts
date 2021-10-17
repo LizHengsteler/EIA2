7972b7c69a6db26b7063b81516be9d110f863de0
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-10-17 18:06:08 +0200

    Namen angepasst

--- Aufgaben/L01/RandomPoem.ts
+++ Aufgaben/L01/RandomPoem.ts
@@ -7,7 +7,7 @@ namespace L01 {
 
     //console.log(subject, verb, object);
     
-    for (let index: number = subject.length; index > 0; index--) {
+    for (let index: number = subject.length; index > 0; index--) { 
 
         getVerse(subject, verb, object);
         //console.log(index);
@@ -16,8 +16,8 @@ namespace L01 {
     function getVerse (_subject: string[], _verb: string[], _object: string[] ): string { //_Platzhalter für Übergabe
         let vers: string = "";
         let randomNumberSubject: number = Math.floor(Math.random() * _subject.length);
-        let randomNumberVerb: number = Math.floor(Math.random() * _subject.length);
-        let randomNumberObject: number = Math.floor(Math.random() * _subject.length);
+        let randomNumberVerb: number = Math.floor(Math.random() * _verb.length);
+        let randomNumberObject: number = Math.floor(Math.random() * _object.length);
         vers = vers + _subject.splice(randomNumberSubject, 1) + " " +  _verb.splice(randomNumberVerb, 1) + " " + _object.splice(randomNumberObject, 1);
         console.log(vers);
         //console.log(_subject);
