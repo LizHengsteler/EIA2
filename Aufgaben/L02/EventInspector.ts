5c5fc1a70046ca04506b542d587addefe77f7732
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-10-28 12:42:07 +0200

    handleLoad Eventlistener

--- Aufgaben/L02/EventInspector.ts
+++ Aufgaben/L02/EventInspector.ts
@@ -1,25 +1,55 @@
 namespace L02_Load {
     window.addEventListener("load", handleLoad);
 
+    let div0: HTMLElement = document.createElement("div");
+    div0.classList.add("div0");
+    document.body.appendChild(div0);
+
+
+    let div1: HTMLElement = document.createElement("div");
+    div1.classList.add("div1");
+    document.body.appendChild(div1);
+
+    let span: HTMLElement = document.createElement("span");
+    span.classList.add("span");
+    document.body.appendChild(span);
+
+  
+
     function handleLoad(_event: Event): void {
+
         console.log(_event);
-        //document.addEventListener("mousemove", )
 
-        let div0: HTMLElement = document.createElement("div");
-        div0.classList.add("div0");
-        document.body.appendChild(div0);
+        //MouseMove Listener
+        document.addEventListener("mousemove", setInfoBox);
+        
+        //click-Listener
+        document.querySelector(".div0").addEventListener("click", logInfo);
+        document.querySelector(".div1").addEventListener("click", logInfo);
+        document.addEventListener("click", logInfo);
+        document.body.addEventListener("click", logInfo);
+        //document.querySelector(".div0").addEventListener("click", logInfo);
 
-
-        let div1: HTMLElement = document.createElement("div");
-        div1.classList.add("div1");
-        document.body.appendChild(div1);
-
-        let span: HTMLElement =document.createElement("span");
-        span.classList.add("span");
-        document.body.appendChild(span);
-       
+        //keyup-Listener
+        document.querySelector(".div0").addEventListener("keyup", logInfo);
+        document.querySelector(".div1").addEventListener("keyup", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("keyup", logInfo);
 
         
+
+      
+        
+  }
+
+    function setInfoBox (_event: MouseEvent): void {
+      span.addEventListener("mousemove", setInfoBox);
+
+    
+  }
+
+  function logInfo (_event: Event) {
+    
   }
 
 
