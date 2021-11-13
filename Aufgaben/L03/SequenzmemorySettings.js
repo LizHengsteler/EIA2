08faac80707a0bcf9429afedd82fef422f8b48a8
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-11-13 13:51:59 +0100

    FormData Auswertung

--- Aufgaben/L03/SequenzmemorySettings.js
+++ Aufgaben/L03/SequenzmemorySettings.js
@@ -2,17 +2,61 @@
 //Zusammenarbeit mit Theo
 var SequenzmemorySettings;
 (function (SequenzmemorySettings) {
-    window.addEventListener("load", startGame);
-    let input;
-    input = document.querySelector("#inputField");
-    let sequenceArray = input.value.split("");
+    window.addEventListener("load", handleLoad);
+    let startButton;
+    let nodeList;
+    let sequenceArray;
     let counter = 0;
+    let frame;
+    let slider;
+    let backgroundColor;
+    let cardColor;
+    let fontColor;
+    let input;
+    let stepperTime;
+    //let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+    //console.log(formData);
+    function handleLoad(_event) {
+        input = document.querySelector("#codeWord");
+        frame = document.querySelector("#formFrame");
+        //let slider: HTMLInputElement = <HTMLInputElement>(
+        document.querySelector("input#size");
+        startButton = document.getElementById("startButton");
+        startButton.addEventListener("click", startGame);
+        frame.addEventListener("change", handleChange);
+        //handleChange(_event);
+    }
+    //console.log(startGame);
+    function handleChange(_event) {
+        /*let inputs: NodeListOf<HTMLInputElement> =
+          document.querySelectorAll("input");
+        console.log(inputs);
+        let font: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
+        console.log(font.value);*/
+        let formData = new FormData(document.forms[0]);
+        //console.log(formData);
+        for (let entry of formData) {
+            console.log(entry);
+        }
+    }
     function startGame() {
-        for (let index = 0; index < sequenceArray.length; index++) {
-            let shuffleNumber = Math.floor(Math.random() * (sequenceArray.length));
+        frame = document.getElementById("formFrame");
+        frame.classList.add("hidden");
+        sequenceArray = input.value.split("");
+        for (let index = 0; index <= sequenceArray.length; index++) {
+            let shuffleNumber = Math.floor(Math.random() * sequenceArray.length);
             console.log(shuffleNumber);
             let createSpan = document.createElement("span");
+            createSpan.innerHTML = sequenceArray[shuffleNumber];
+            sequenceArray.splice(shuffleNumber, 1);
+            console.log(sequenceArray);
         }
+        setTimeout(function () {
+            hideCards();
+        }, 2000);
+    }
+    function hideCards() {
+        console.log("Hello");
     }
 })(SequenzmemorySettings || (SequenzmemorySettings = {}));
 //# sourceMappingURL=SequenzmemorySettings.js.map
--- Aufgaben/L03/SequenzmemorySettings.ts
+++ Aufgaben/L03/SequenzmemorySettings.ts
@@ -1,17 +1,62 @@
 //Zusammenarbeit mit Theo
 namespace SequenzmemorySettings {
-  let startButton: HTMLButtonElement;
-  startButton = <HTMLButtonElement>document.querySelector("#startButton");
-  startButton.addEventListener("click", startGame);
-  console.log(startGame);
+  window.addEventListener("load", handleLoad);
 
-  let input: HTMLInputElement;
-  input = <HTMLInputElement>document.querySelector("#inputField");
-  let sequenceArray: string[] = input.value.split("");
+  let startButton: HTMLButtonElement;
+  let nodeList: NodeListOf<HTMLInputElement>;
+  let sequenceArray: string[];
   let counter: number = 0;
+  let frame: HTMLDivElement;
+  let slider: HTMLInputElement;
+  let backgroundColor: HTMLInputElement;
+  let cardColor: HTMLInputElement;
+  let fontColor: HTMLInputElement;
+  let input: HTMLInputElement;
+  let stepperTime: HTMLInputElement;
+
+  
+
+  //let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+  //console.log(formData);
+
+  function handleLoad(_event: Event): void {
+    
+    input = <HTMLInputElement>document.querySelector("#codeWord");
+    frame = <HTMLDivElement>document.querySelector("#formFrame");
+    
+    //let slider: HTMLInputElement = <HTMLInputElement>(
+    document.querySelector("input#size");
+    startButton = <HTMLButtonElement>document.getElementById("startButton");
+    startButton.addEventListener("click", startGame);
+    frame.addEventListener("change", handleChange);
+    
+    //handleChange(_event);
+  
+  }
+  //console.log(startGame);
+
+  
+
+  function handleChange(_event: Event): void {
+    /*let inputs: NodeListOf<HTMLInputElement> =
+      document.querySelectorAll("input");
+    console.log(inputs);
+    let font: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
+    console.log(font.value);*/
+    let formData: FormData = new FormData(document.forms[0]);
+    //console.log(formData);
+    for (let entry of formData) {
+      console.log(entry);
+    }
+  }
+  
 
   function startGame(): void {
-    for (let index: number = 0; index < sequenceArray.length; index++) {
+    frame = <HTMLDivElement>document.getElementById("formFrame");
+    frame.classList.add("hidden");
+    sequenceArray = input.value.split("");
+
+    for (let index: number = 0; index <= sequenceArray.length; index++) {
       let shuffleNumber: number = Math.floor(
         Math.random() * sequenceArray.length
       );
@@ -19,6 +64,18 @@ namespace SequenzmemorySettings {
       let createSpan: HTMLSpanElement = document.createElement("span");
       createSpan.innerHTML = sequenceArray[shuffleNumber];
       sequenceArray.splice(shuffleNumber, 1);
+      console.log(sequenceArray);
     }
+    setTimeout(function (): void {
+      hideCards();
+    },         2000 );
+
+  }
+
+  function hideCards(): void {
+    console.log("Hello");
+
+    
   }
 }
+
