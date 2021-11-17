1d17f4c78fa5c8f635f5b20d0abfb6c761caf9dd
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-11-17 16:58:34 +0100

    bunter Ball

--- Aufgaben/L08/GenerativeKunst.js
+++ Aufgaben/L08/GenerativeKunst.js
@@ -2,8 +2,8 @@
 var GenerativeKunst;
 (function (GenerativeKunst) {
     let crc2;
-    let x = 0;
-    let y = 0;
+    let x;
+    let y;
     window.addEventListener("load", hndLoad);
     function hndLoad(_event) {
         let canvas = document.querySelector("canvas");
@@ -11,32 +11,57 @@ var GenerativeKunst;
         canvas.height = window.innerHeight;
         //console.log(canvas);
         crc2 = canvas.getContext("2d");
-        drawBall();
-        /*for (let i: number = 0; i < 100; i++) {
-          let x: number = Math.random() * 400;
-          let y: number = Math.random() * 300;
-          drawBall();
+        for (let i = 0; i < 100; i++) {
+            x = Math.random() * 100;
+            y = Math.random() * 100;
         }
-      }*/
-        function drawBall() {
-            let dx = 2;
-            let dy = 2;
-            let radius = 30;
-            window.requestAnimationFrame(drawBall);
-            //crc2.clearRect(0, 0, innerWidth, innerHeight);
-            crc2.beginPath();
-            crc2.arc(x, y, radius, 0, Math.PI * 2, false);
-            crc2.strokeStyle = "#ffffff";
-            crc2.stroke();
-            if (x + radius > innerWidth && x - radius < 0) {
-                dx = -dx;
-            }
-            if (y + radius > innerHeight && y - radius < 0) {
-                dy = -dy;
-            }
-            x += dx * 5;
-            y += dy * 5;
+        drawBalls();
+        drawBouncingBall();
+    }
+    function drawBalls() {
+        for (let i = 0; i < 100; i++) {
+            x = Math.random() * 100;
+            y = Math.random() * 100;
         }
+        let dx = 2;
+        let dy = 2;
+        let radius = 40;
+        //window.requestAnimationFrame(drawBalls);
+        //crc2.clearRect(0, 0, innerWidth, innerHeight);
+        crc2.beginPath();
+        crc2.arc(x, y, radius, 0, Math.PI * 2, false);
+        crc2.strokeStyle = "#ffffff";
+        crc2.stroke();
+        if (x + radius > innerWidth && x - radius < 0) {
+            dx = -dx;
+        }
+        if (y + radius > innerHeight && y - radius < 0) {
+            dy = -dy;
+        }
+        x += dx * 2;
+        y += dy * 2;
+    }
+    function drawBouncingBall() {
+        let dx = 2;
+        let dy = 2;
+        let radius = 40;
+        window.requestAnimationFrame(drawBouncingBall);
+        let purple = Math.floor(Math.random() * 200);
+        let yellow = Math.floor(Math.random() * 400);
+        let blue = Math.floor(Math.random() * 600);
+        //crc2.clearRect(0, 0, innerWidth, innerHeight);
+        crc2.beginPath();
+        crc2.arc(x, y, radius, 0, Math.PI * 2, false);
+        crc2.strokeStyle = "rgb(" + purple + ", " + yellow + "," + blue + ")";
+        crc2.stroke();
+        if (x + radius > innerWidth && x - radius < 0) {
+            dx = -dx;
+        }
+        if (y + radius > innerHeight && y - radius < 0) {
+            dy = -dy;
+        }
+        x += dx * 2;
+        y += dy * 2;
     }
 })(GenerativeKunst || (GenerativeKunst = {}));
 //# sourceMappingURL=GenerativeKunst.js.map
--- Aufgaben/L08/GenerativeKunst.ts
+++ Aufgaben/L08/GenerativeKunst.ts
@@ -1,9 +1,7 @@
 namespace GenerativeKunst {
-
   let crc2: CanvasRenderingContext2D;
-  let x: number = 0;
-  let y: number = 0;
-  
+  let x: number;
+  let y: number;
 
   window.addEventListener("load", hndLoad);
 
@@ -15,44 +13,69 @@ namespace GenerativeKunst {
 
     crc2 = canvas.getContext("2d")!;
 
-    drawBall();
-
-    /*for (let i: number = 0; i < 100; i++) {
-      let x: number = Math.random() * 400;
-      let y: number = Math.random() * 300;
-      drawBall();
-    }
-  }*/
-  
-
-    function drawBall(): void {
-        let dx: number = 2;
-        let dy: number = 2;
-        let radius: number = 30;
-
-
-        window.requestAnimationFrame(drawBall);
+    for (let i: number = 0; i < 100; i++) {
+        x = Math.random() * 100;
+        y = Math.random() * 100;
+      }
+    drawBalls(); 
     
-    //crc2.clearRect(0, 0, innerWidth, innerHeight);
-        crc2.beginPath();
-        crc2.arc(x, y, radius, 0, Math.PI * 2, false);
-        crc2.strokeStyle = "#ffffff";
-        crc2.stroke();
-    
-        if (x + radius > innerWidth && x - radius < 0) {
-         dx = -dx;
-        }
-        if (y + radius > innerHeight && y - radius < 0) {
-        dy = -dy;
-       }
-        x += dx * 5;
-        y += dy * 5;
-
-   
+    drawBouncingBall();
   }
 
+  function drawBalls (): void {
+    for (let i: number = 0; i < 100; i++) {
+        x = Math.random() * 100;
+        y = Math.random() * 100;
+      }
+    let dx: number = 2;
+    let dy: number = 2;
+    let radius: number = 40;
 
+    //window.requestAnimationFrame(drawBalls);
 
+    //crc2.clearRect(0, 0, innerWidth, innerHeight);
+    
+    crc2.beginPath();
+    crc2.arc(x, y, radius, 0, Math.PI * 2, false);
+    crc2.strokeStyle = "#ffffff";
+    crc2.stroke();
 
-}
+    if (x + radius > innerWidth && x - radius < 0) {
+      dx = -dx;
+    }
+    if (y + radius > innerHeight && y - radius < 0) {
+      dy = -dy;
+    }
+    x += dx * 2;
+    y += dy * 2;
+      
+  }
+
+  function drawBouncingBall(): void { //Warum bounct er nicht an der Wand ab?
+
+    
+    let dx: number = 2;
+    let dy: number = 2;
+    let radius: number = 40;
+
+    window.requestAnimationFrame(drawBouncingBall);
+    let purple: number = Math.floor(Math.random() * 200);
+    let yellow: number = Math.floor(Math.random() * 400);
+    let blue: number = Math.floor(Math.random() * 600);
+
+    //crc2.clearRect(0, 0, innerWidth, innerHeight);
+    crc2.beginPath();
+    crc2.arc(x, y, radius, 0, Math.PI * 2, false);
+    crc2.strokeStyle = "rgb(" + purple + ", " + yellow + "," + blue + ")";
+    crc2.stroke();
+
+    if (x + radius > innerWidth && x - radius < 0) {
+      dx = -dx;
+    }
+    if (y + radius > innerHeight && y - radius < 0) {
+      dy = -dy;
+    }
+    x += dx * 2;
+    y += dy * 2;
+  }
 }
